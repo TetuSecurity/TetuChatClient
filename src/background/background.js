5353f78d63fbf7e45e7b006f0faf41da31be6b8b
Adam Yost <swimmadude66@gmail.com>
2016-03-05 17:51:48 -0500

    encode the key as a string for easier transmittal

--- src/background/background.js
+++ src/background/background.js
@@ -5,7 +5,7 @@ window.onload = function () {
 	ipc.on('encrypt-request', function(event, envelope){
     var enc_obj = encryption.encrypt(envelope.Data);
 		envelope.Data = enc_obj.Data;
-    envelope.Key = encryption.RSAencrypt(enc_obj.Key, envelope.PublicKey);
+    envelope.Key = encryption.RSAencrypt(enc_obj.Key, envelope.PublicKey).toString('hex');
 		delete envelope.PublicKey;
     envelope.Signature = encryption.sign(enc_obj.Data);
     ipc.send('encrypt-response', envelope);
@@ -15,7 +15,7 @@ window.onload = function () {
     if(encryption.verify(envelope.Data, envelope.Signature, envelope.PublicKey)){
 			delete envelope.Signature;
 			delete envelope.PublicKey;
-      var deckey = encryption.RSAdecrypt(envelope.Key);
+      var deckey = encryption.RSAdecrypt(new Buffer(envelope.Key, 'hex'));
 			delete  envelope.Key;
       envelope.Data = encryption.decrypt(envelope.Data, deckey);
       ipc.send('decrypt-response', envelope);
